9cddfa0d55ae5983f8a3206a7c792c3a7fafd35e
MullaN <admullan@gmail.com>
2021-01-26 15:59:02 -0800

    second lesson

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,15 +7,26 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
+
         Debug.Log("Welcome to Number Wizards.");
         Debug.Log("Please pick a number");
-        Debug.Log("The highest number is: 1000");
-        Debug.Log("The lowest number is: 0");
+        Debug.Log("The highest number is: " + max);
+        Debug.Log("The lowest number is: " + min);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.UpArrow)){
+            Debug.Log("Up Key was pressed.");
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow)){
+            Debug.Log("Down Key was pressed.");
+        }
+        else if(Input.GetKeyDown(KeyCode.Return)){
+            Debug.Log("Return was pressed.");
+        }
     }
 }
